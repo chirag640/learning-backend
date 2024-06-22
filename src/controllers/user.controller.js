d8fffe44aa84fd081a406d119b975759109df119
chirag640 <chaudharychirag640@gmail.com>
2024-06-22 11:07:29 +0530

    working on controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,54 @@
-import { asyncHandler } from '../utils/asyncHandler.js'
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary} from '../utils/cloudinary.js'
+import { ApiResponse } from "../utils/ApiResponse.js";
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, userName, password } = req.body;
+  console.log("Email :", email);
 
-const registerUser = asyncHandler(async(req , res)=>{
-    res.status(200).json({
-        message:"OK"
-    })
-})
+  if (
+    [fullName, email, userName, password].some((feild) => feild?.trim() === "")
+  ) {
+    throw new ApiError(400, "All feild are compolsory");
+  }
+  const existedUser = User.findOne({
+    $or: [{ userName }, { email }],
+  });
 
-export { registerUser }
+  if (existedUser) {
+    throw new ApiError(409, "user already exist");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if(!avatarLocalPath){
+    throw new ApiError(400,"avatar file is required")
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const covereImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!avatar){
+    throw new ApiError(400,"avatar file is required")
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar:avatar.url,
+    coverImage:covereImage?.url || "",
+    email,
+    password,
+    userName:userName.toLowerCase(),
+  })
+
+  const userCreated = await User.findById(user._id).select("-password -refreshTocken")
+  if(!userCreated){
+    throw new ApiError(500, "something went wrong while registering user")
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200,userCreated , "user registerd successfully")
+  )
+});
+
+export { registerUser };
